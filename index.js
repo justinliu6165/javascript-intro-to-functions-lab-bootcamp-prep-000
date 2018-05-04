e653053977d0171e5d8408dbf7103ca47b084156
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-04 17:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  if (string===string.toLowerCase())
+  if (string.toLowerCase()===string)
   {
     return console.log("I can\'t hear you!")
   }
